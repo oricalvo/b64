2aa086d204b05d7913264ecf38cc2cbc5655d3ce
ocalvo <ori.calvo@gmail.com>
2017-10-20 12:24:35 +0300

    Adding install command

--- setup.cmd
+++ setup.cmd
@@ -1,2 +1,3 @@
 c:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /out:tools\fromb64.exe src\fromb64.cs
 c:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /out:tools\tob64.exe src\tob64.cs
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /out:tools\install.exe src\install.cs
---
+++ src/install.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace install
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Missing URL or file path");
+                return -1;
+            }
+
+            string package = args[0];
+
+            try
+            {
+                string rootDir = Path.GetDirectoryName(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+                string baseUrl = "https://github.com/oricalvo/b64/raw/master/packages/";
+                string fileName = package + ".b64";
+                string url = baseUrl + fileName;
+                WebClient client = new WebClient();
+                string dirPath = Path.Combine(rootDir, "packages");
+                if (!Directory.Exists(dirPath))
+                {
+                    Directory.CreateDirectory(dirPath);
+                }
+                string filePath = Path.Combine(dirPath, fileName);
+                client.DownloadFile(url, filePath);
+
+                string text = File.ReadAllText(filePath);
+                byte[] bytes = Convert.FromBase64String(text);
+                string target = Path.Combine(dirPath, package);
+                File.WriteAllBytes(target, bytes);
+
+				File.Delete(filePath);
+				
+                return 0;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.ToString());
+                return -2;
+            }
+        }
+    }
+}
